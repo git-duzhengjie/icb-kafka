3e2ae07511e408370746ca9e104ab357837ad185
duzhengjie <zhengjie.du@foxmail.com>
2018-06-04 10:24:49 +0800

    添加项目文件。

---
+++ kafka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
